5000a1bc48cc0a34e4aa1e3c1268e690d1313e49
magzhan <magzhanseidin@gmail.com>
2025-05-01 18:37:42 +0500

    after changes

--- eventpr/settings.py
+++ eventpr/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-qx-styd2lr-m7rr)p&p_9rj7nm@cj@52sje=kxt@12wnr^1x6%
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 
